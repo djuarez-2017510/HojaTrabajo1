1da9a396ba3dd89f41edbeeafa6a99d00592582c
andresm220 <123190520+andresm220@users.noreply.github.com>
2024-01-13 22:47:41 -0600

    Funciones de guardar y seleccionar estacion guardada

--- HRadio/Main.java
+++ HRadio/Main.java
@@ -20,6 +20,7 @@ public class Main {
             System.out.println("0. Salir");
 
             int choice = scanner.nextInt();
+            
 
             switch (choice) {
                 case 1:
@@ -33,15 +34,189 @@ public class Main {
                 }
                     break;
                 case 3:
+
                 if(miradio.isOn()){
+                    
                     System.out.println("La estación actual: " + miradio.nextStation());
                 }else{
                     System.out.println("La radio no esta encendida");
                 }
                     break;
                 case 4:
+                    System.out.println("En que boton desea guardarlo(Seleccione un numero del 1 al 12)");
+                    int choice2= scanner.nextInt();
+                    switch(choice2){
+                        case 1:
+                        if(miradio.isAM()){
+                             miradio.SaveStation(0, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(0, miradio.numberFM);
+
+                        }
+                        break;
+                        case 2:
+                         if(miradio.isAM()){
+                             miradio.SaveStation(1, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(1, miradio.numberFM);
+
+                        }
+                        break;
+                        case 3:
+                         if(miradio.isAM()){
+                             miradio.SaveStation(2, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(2, miradio.numberFM);
+
+                        }
+                        break;
+                        case 4:
+                         if(miradio.isAM()){
+                             miradio.SaveStation(3, miradio.numberAM);
+                        }else{
+                            miradio.SaveStation(3, miradio.numberFM);
+
+                        }
+                        break;
+                        case 5:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(4, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(4, miradio.numberFM);
+
+                        }
+                        break;
+                        case 6:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(5, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(5, miradio.numberFM);
+
+                        }
+                        break;
+                        case 7:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(6, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(6, miradio.numberFM);
+
+                        }
+                        break;
+                        case 8:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(7, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(7, miradio.numberFM);
+
+                        }
+                        break;
+                        case 9:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(8, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(8, miradio.numberFM);
+
+                        }
+                        break;
+                        case 10:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(9, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(9, miradio.numberFM);
+
+                        }
+                        break;
+                        case 11:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(10, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(10, miradio.numberFM);
+
+                        }
+                        break;
+                        case 12:
+                          if(miradio.isAM()){
+                             miradio.SaveStation(11, miradio.numberAM);
+
+                        }else{
+                            miradio.SaveStation(11,miradio.numberFM);
+
+                        }
+                        break;
+                        
+
+
+                        default:
+                        System.out.println("No existe un boton con ese numero");
+
+                       
+                    }
+
                     break;
                 case 5:
+                 
+                 System.out.println("¿A qué boton quiere acceder?");
+                 int choice3= scanner.nextInt();
+                 switch (choice3) {
+                    case 1:
+                    miradio.SelectStation(0);
+                        
+                        break;
+                    case 2:
+                     miradio.SelectStation(1);
+
+
+
+                    break;
+                    case 3:
+                     miradio.SelectStation(2);
+                    break;
+                    case 4:
+                     miradio.SelectStation(3);
+                    break;
+                    case 5:
+                     miradio.SelectStation(4);
+                    break;
+                    case 6:
+                     miradio.SelectStation(5);
+                    break;
+                    case 7:
+                     miradio.SelectStation(6);
+                    break;
+                    case 8:
+                     miradio.SelectStation(7);
+                    break;
+                    case 9:
+                     miradio.SelectStation(8);
+                    break;
+                    case 10:
+                     miradio.SelectStation(9);
+                    break;
+                    case 11:
+                     miradio.SelectStation(10);
+                    break;
+                    case 12:
+                     miradio.SelectStation(11);
+                    break;
+
+                
+                    default:
+                    System.out.println("No existe un boton al cual acceder");
+                        break;
+                }
+
+                
+                
                     break;
                 case 0:
                 System.out.println("Saliendo del programa");
--- HRadio/Radio.java
+++ HRadio/Radio.java
@@ -3,16 +3,16 @@ public class Radio implements IRadio {
     private double[] ArrayAM=new double[11];
     private double[] ArrayFM=new double[11];
     private boolean amfm;
-    private double numberAM;
-    private double numberFM;
+    protected double numberAM=530.0;
+    protected double numberFM=87.9;
 
     public Radio(){
         // en la variable amfm false reprsenta que esta AM y true que esta FM
         amfm=false;
         //en la variable state true representa encendido y false apagado
         state=false;
-        numberAM = 530.0;
-        numberFM = 87.9;
+     
+
     }
    public void SwitchOnOff(){
         if(state==true){
@@ -44,7 +44,7 @@ public class Radio implements IRadio {
 
     public double nextStation(){
         if(isAM()){
-            numberAM += 10.0;
+            numberAM += 100.0;
             if(numberAM > 1610.0){
                 numberAM = 530.0;
             }
@@ -56,6 +56,45 @@ public class Radio implements IRadio {
             }
             return numberFM;
         }
+    }
+    public void SaveStation(int id, double number) {
+        if (isAM()) {
+            if (id >= 0 && id < ArrayAM.length) {
+                ArrayAM[id] = number;
+            } else {
+                System.out.println("Posición no válida para la frecuencia AM.");
+            }
+        } else {
+            if (id >= 0 && id < ArrayFM.length) {
+                ArrayFM[id] = number;
+            } else {
+                System.out.println("Posición no válida para la frecuencia FM.");
+            }
+        }
+    }
+
+    public double SelectStation(int id) {
+        double estacionSeleccionada = 0.0;
+
+        if (isAM()) {
+            if (id >= 0 && id < ArrayAM.length) {
+                estacionSeleccionada = ArrayAM[id];
+                System.out.println("La radio se ha movido a la estacion asignada en el boton " + id + " en la frecuencia AM: " + estacionSeleccionada);
+                numberAM = estacionSeleccionada;
+            } else {
+                System.out.println("Posición no válida para la frecuencia AM.");
+            }
+        } else {
+            if (id >= 0 && id < ArrayFM.length) {
+                estacionSeleccionada = ArrayFM[id];
+                System.out.println("La radio se ha movido a la estacion asignada en el boton " + id + " en la frecuencia FM: " + estacionSeleccionada);
+                numberFM = estacionSeleccionada;
+            } else {
+                System.out.println("Posición no válida para la frecuencia FM.");
+            }
+        }
+
+        return estacionSeleccionada;
     }
 
 }
