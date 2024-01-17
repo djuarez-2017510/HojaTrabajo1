f2d494cafb3f48ba72e1ff9e55a209762db2b5e3
djuarez-2017510 <djuarez-2017510@kinal.edu.gt>
2024-01-16 19:34:11 -0600

    Ultimos cambios

--- HRadio2/IRadio.java
+++
@@ -1,9 +0,0 @@
-public interface IRadio {
-    public void saveStation(int id,double station);
-    public boolean isAm();
-    public boolean isOn();
-    public double selectStation(int id);
-    public void switchOnOff();
-    public void switchAMFM();
-    public double nextStation();
- }
